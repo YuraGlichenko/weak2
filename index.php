8982196b618a4e95ed671ed1dfd60a10c9462297
YuraGlichenko <relekviym@rambler.ru>
2019-07-29 03:19:11 +0300

    02.file index.php remote comment

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'src/function.php';
 
-//task1('data.xml');
-//task2();
-//task3();
+task1('data.xml');
+task2();
+task3();
 task4('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
